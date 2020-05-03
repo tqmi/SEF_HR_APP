7e64d51c3e2d0dd2f1384d2e742a1e66f20d30a7
tqmi <tqmi_szasz@yahoo.com>
2020-05-03 15:00:34 +0300

    Part of last commit
    SHA-9

---
+++ src/main/java/SEF_HR_APP/backend/database/DBEntry.java
@@ -0,0 +1,9 @@
+package SEF_HR_APP.backend.database;
+
+public interface DBEntry {
+    
+    public String[] getFieldsName();
+    public String[] getFieldsData();
+    public String[] getFieldsType();
+
+}
