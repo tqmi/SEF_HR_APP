7cc74e57ed429c1e915ec54544ab28f3052b0267
tqmi <tqmi_szasz@yahoo.com>
2020-05-10 22:12:06 +0300

    Implemented functionality where requesting and providing activity cant be done for more than the latest 6 respectively 2 months
    SHA-29

--- src/main/java/SEF_HR_APP/backend/datamodels/activity/MonthType.java
+++ src/main/java/SEF_HR_APP/backend/datamodels/activity/MonthType.java
@@ -1,28 +1,45 @@
 package SEF_HR_APP.backend.datamodels.activity;
 
+import java.util.Calendar;
+
 public enum MonthType {
-    JAN("JAN"),
-    FEB("FEB"),
-    MAR("MAR"),
-    APR("APR"),
-    MAY("MAY"),
-    IUN("IUN"),
-    IUL("IUL"),
-    AUG("AUG"),
-    SEP("SEP"),
-    OCT("OCT"),
-    NOV("NOV"),
-    DEC("DEC");
+    JAN("JAN",0),
+    FEB("FEB",1),
+    MAR("MAR",2),
+    APR("APR",3),
+    MAY("MAY",4),
+    IUN("IUN",5),
+    IUL("IUL",6),
+    AUG("AUG",7),
+    SEP("SEP",8),
+    OCT("OCT",9),
+    NOV("NOV",10),
+    DEC("DEC",11);
     
 
     private String representation;
+    private int index;
 
-    private MonthType(String rep){
+
+    private MonthType(String rep,int index){
         representation = rep;
+        this.index = index;
     }
 
     public String getStringRepresentation(){
         return representation;
+    }
+
+    public int monthDifferenceToCurrent(){
+        
+        int other = Calendar.getInstance().get(Calendar.MONTH);
+        
+        int res = other - index;
+
+        if(res < 0)
+            res += 12;
+
+        return res;
     }
     
 }
